e054469ac892b1ec6ede5053bacdd4365f2a6308
ashvath-nwo <mashaultrafunkthecoder@gmail.com>
2025-04-12 23:14:23 +0530

    Update PulseXFCE4.sh

--- scripts/PulseXFCE4.sh
+++ scripts/PulseXFCE4.sh
@@ -3,6 +3,7 @@ pkg install x11-repo -y
 pkg install termux-x11-nightly -y
 pkg install xfce4 -y
 pkg install xfce4-terminal -y
+pkg install xfce4-session -y
 curl -o setup.sh https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/installpulseaudio.sh && chmod +rwx setup.sh && ./setup.sh && rm setup.sh
 echo 'termux-x11 :1 -xstartup "dbus-launch --exit-with-session xfce4-session"' > $PREFIX/bin/start-termux-x11 && chmod +x $PREFIX/bin/start-termux-x11
 echo Pulseaudio and XFCE4 is installed. Restart the app to activate Pulseaudio. to start XFCE4 enter the command start-termux-x11
