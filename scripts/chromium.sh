d07cdfe001a3d3482693f51b3e0d2733866ff4b4
Ashvath <mashaultrafunkthecoder@gmail.com>
2026-09-24 22:46:42 +0530

    Update chromium.sh

--- scripts/chromium.sh
+++ scripts/chromium.sh
@@ -1,7 +1,7 @@
+# Installation Script. now more simple. 24 September 2026 updated.
 apt update
-pkg install tur-repo
-pkg install x11-repo
-apt search browser
-apt install chromium 
+apt install tur-repo -y
+apt install x11-repo -y
+apt install chromium -y
 echo Chromium is installed. You can use it in Desktop environments like XFCE4 using Termux x11
 echo If you want to install XFCE4 as desktop environment with audio Copy the Pulseaudio and XFCE4 script
